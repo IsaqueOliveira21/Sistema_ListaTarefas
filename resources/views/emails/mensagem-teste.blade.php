671c5598a0293d969b6dd55661d615c2bc3e5c32
ZaaK/ <68238387+ZaaKSCR@users.noreply.github.com>
2023-01-24 09:45:46 -0300

    Implementando reset de senhas e envio de emails SMTP

---
+++ resources/views/emails/mensagem-teste.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Introdução
+
+O corpo da mensagem.
+
+<x-mail::button :url="''">
+Botão
+</x-mail::button>
+
+Agradecimentos,<br>
+{{ config('app.name') }}
+</x-mail::message>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Mail\MensagemTesteMail;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,3 +12,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController'); //->middleware('auth');
+
+Route::get('/mensagem-test', function () {
+    //return new MensagemTesteMail();
+    Mail::to('isaque16.oliveira@gmail.com')->send(new MensagemTesteMail()); // Pode ser utilizado no tinker tambem
+    return 'E-mail enviado com sucesso!';
+});
